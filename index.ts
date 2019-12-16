1e7809a3bfbb7321e1204469e971fd2174c33cd4
Jonathon Judge <jjudge@alwayseducation.com>
2019-12-16 13:57:34 -0500

    test push

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const SECRET = config.get("githubkey") ;
       const isMaster = body?.ref === 'refs/heads/master';
       if (isAllowed && isMaster) {
           exec ('cd /home/fgrayli/projects/Node && npm run compile && npm run start');
-        console.log("this is a test");
+        console.log("this is a fart");
       }
     });
     res.end();
