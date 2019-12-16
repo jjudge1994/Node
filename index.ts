cceb914e18c7fb4a280917edc68d9c10cc1f27bf
Jonathon Judge <jjudge@alwayseducation.com>
2019-12-16 14:11:22 -0500

    Test

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const SECRET = config.get("githubkey") ;
       const isMaster = body?.ref === 'refs/heads/master';
       if (isAllowed && isMaster) {
           exec ('cd /home/fgrayli/projects/Node && bash deoploy.sh');
-        console.log("this is a fart");
+        console.log("this is another test");
       }
     });
     res.end();
