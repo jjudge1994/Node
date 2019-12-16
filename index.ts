70cef8959e3c6477fb39f4f65659ea6bc7997b7b
Jonathon Judge <jjudge@alwayseducation.com>
2019-12-16 13:18:17 -0500

    Test

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { createServer } from "http";
 import { createHmac } from "crypto";
 import * as config from "config";
+import { exec } from "child_process";
 const SECRET = config.get("githubkey") ;
 
   createServer((req, res) => {
@@ -13,6 +14,7 @@ const SECRET = config.get("githubkey") ;
       const body = JSON.parse(chunk);
       const isMaster = body?.ref === 'refs/heads/master';
       if (isAllowed && isMaster) {
+          exec ('cd /home/fgrayli/projects/Node && npm run compile && npm run start');
         console.log("this is a fart");
       }
     });
