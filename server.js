c56d20618bdbd77a5df153063edceca6b52212f4
Jonathon Judge <jjudge@alwayseducation.com>
2019-12-17 13:41:53 -0500

    Test Push

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Testing 123456789!');
+  res.send('Testing farts!');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
